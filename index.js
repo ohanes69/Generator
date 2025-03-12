e716a7f036826a3f19b14ee48e4cd0fd222c2850
Sacha Markarian <markarian696@icloud.com>
2025-03-12 17:16:00 +0100

    change 😃

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-const menuToggle = document.getElementById('menu-toggle');
-const mobileMenu = document.getElementById('mobile-menu');
-const menuClose = document.getElementById('menu-close');
+// Support
+const clickSupport = document.getElementById('clickSupport')
+clickSupport.addEventListener('click', () => {
+    try {
+        window.location.href = 'mailto:billify@gmail.com';
+    } catch (error) {
+        alert("Veuillez configurer une application de messagerie pour envoyer un e-mail.");
+    }
+})
 
-menuToggle.addEventListener('click', () => {
-    mobileMenu.classList.remove('-translate-x-full');
+const clickSupportMobile = document.getElementById('clickSupportMobile')
+clickSupportMobile.addEventListener('click', () => {
+    try {
+        window.location.href = 'mailto:billify@gmail.com';
+    } catch (error) {
+        alert("Veuillez configurer une application de messagerie pour envoyer un e-mail. Sinon contactez-nous sur billify@gmail.com");
+    }
+})
+
+// MenuVersionDesk&Mob
+document.addEventListener("DOMContentLoaded", function () {
+    const buttonBurger = document.getElementById("buttonBurger");
+    const buttonClose = document.getElementById("buttonClose");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    function toggleMenu() {
+        mobileMenu.classList.toggle("-translate-y-full");
+        buttonBurger.classList.toggle("hidden");
+        buttonClose.classList.toggle("hidden");
+    }
+
+    // Écouteurs d'événements pour l'ouverture et la fermeture
+    buttonBurger.addEventListener("click", toggleMenu);
+    buttonClose.addEventListener("click", toggleMenu);
 });
 
-menuClose.addEventListener('click', () => {
-    mobileMenu.classList.add('-translate-x-full');
+// Pour le scrool, CDN
+document.addEventListener("DOMContentLoaded", () => {
+    AOS.init();
 });
