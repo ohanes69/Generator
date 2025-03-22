6053c86a8d23bcb2becdacac1b40fdfc9426e995
Sacha Markarian <markarian696@icloud.com>
2025-03-22 03:23:57 +0100

    create legalmention, cgu&cgv

---
+++ legalmentions.js
@@ -0,0 +1,50 @@
+// Support
+const clickSupport = document.getElementById('clickSupport')
+clickSupport.addEventListener('click', () => {
+    try {
+        window.location.href = 'mailto:billify@gmail.com';
+    } catch (error) {
+        alert("Veuillez configurer une application de messagerie pour envoyer un e-mail.");
+    }
+})
+
+const clickSupportMobile = document.getElementById('clickSupportMobile')
+clickSupportMobile.addEventListener('click', () => {
+    try {
+        window.location.href = 'mailto:billify@gmail.com';
+    } catch (error) {
+        alert("Veuillez configurer une application de messagerie pour envoyer un e-mail. Sinon contactez-nous sur billify@gmail.com");
+    }
+})
+
+// MenuVersionDesk&Mob
+document.addEventListener("DOMContentLoaded", function () {
+    const buttonBurger = document.getElementById("buttonBurger");
+    const buttonClose = document.getElementById("buttonClose");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    function toggleMenu() {
+        // Basculer l'affichage du menu
+        mobileMenu.classList.toggle("-translate-y-full");
+
+        // Basculer la visibilité des boutons
+        buttonBurger.classList.toggle("hidden");
+        buttonClose.classList.toggle("hidden");
+
+        // Activer/désactiver le défilement de la page
+        if (mobileMenu.classList.contains("-translate-y-full")) {
+            document.body.classList.remove("overflow-hidden"); // Réactiver le défilement
+        } else {
+            document.body.classList.add("overflow-hidden"); // Désactiver le défilement
+        }
+    }
+
+    // Écouteurs d'événements pour l'ouverture et la fermeture
+    buttonBurger.addEventListener("click", toggleMenu);
+    buttonClose.addEventListener("click", toggleMenu);
+});
+
+// Pour le scrool, CDN
+document.addEventListener("DOMContentLoaded", () => {
+    AOS.init();
+});
