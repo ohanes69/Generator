613f50ddb9f7edb18b3cc95c8efedcd755ae2685
Sacha Markarian <markarian696@icloud.com>
2025-03-27 17:08:14 +0100

    c

--- index.js
+++ index.js
@@ -53,15 +53,10 @@ document.addEventListener("DOMContentLoaded", () => {
 // Changer "choisir le fichier" en nom de l'image
 function updateFileName() {
     const fileInput = document.getElementById('uploadLogo');
-    const fileButton = document.getElementById('fileBase');
+    const fileNameSpan = document.getElementById('fileName');
+    const fileName = fileInput.files[0] ? fileInput.files[0].name : 'Aucun fichier choisi';
     
-    // Vérifier si un fichier a été sélectionné
-    if (fileInput.files.length > 0) {
-        const fileName = fileInput.files[0].name;
-        fileButton.textContent = fileName;  // Remplacer le texte du bouton par le nom du fichier
-    } else {
-        fileButton.textContent = 'Choisir le fichier'; // Rétablir le texte par défaut si aucun fichier n'est sélectionné
-    }
+    fileNameSpan.textContent = fileName;
 }
 
 // Changer le défilement de <options>
@@ -82,3 +77,48 @@ optionsPayment.addEventListener('click', () => {
     }
 })
 
+// If I select an option, text pop
+const textBase = document.getElementById('textBase');
+const paymentOptions = document.querySelectorAll('#optionsList button');
+
+// Fonction pour mettre à jour le texte
+paymentOptions.forEach(button => {
+    button.addEventListener('click', () => {
+        textBase.textContent = button.textContent; // Remplace le texte
+        document.getElementById('optionsList').classList.add('hidden'); // Ferme le menu après sélection
+    });
+});
+
+
+
+
+
+// Changer le défilement de <options>
+const optionsPayment2 = document.getElementById('optionsPayment2')
+const optionsList2 = document.getElementById('optionsList2')
+const optionsBorder2 = document.getElementById('optionsBorder2')
+
+optionsPayment2.addEventListener('click', () => {
+    if (optionsList.classList.contains('hidden'))
+    {
+        optionsList2.classList.remove('hidden')
+        optionsBorder2.classList.replace('rounded-lg', 'rounded-t-lg')
+    }
+    else 
+    {
+        optionsList2.classList.add('hidden')
+        optionsBorder2.classList.replace('rounded-t-lg', 'rounded-lg')
+    }
+})
+
+// If I select an option, text pop
+const textBase2 = document.getElementById('textBase2');
+const paymentOptions2 = document.querySelectorAll('#optionsList2 button');
+
+// Fonction pour mettre à jour le texte
+paymentOptions2.forEach(button => {
+    button.addEventListener('click', () => {
+        textBase2.textContent = button.textContent; // Remplace le texte
+        document.getElementById('optionsList2').classList.add('hidden'); // Ferme le menu après sélection
+    });
+});
